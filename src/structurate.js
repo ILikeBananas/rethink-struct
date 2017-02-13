e220f8d2499a1c10623b0c1ac2d9b01b2bcb4488
Xavier Stouder <xavier@stouder.io>
2017-02-13 22:32:35 +0100

    Add some assertations

--- src/structurate.js
+++ src/structurate.js
@@ -1,19 +1,34 @@
+const assert = require('assert')
 const rethink = require('rethinkdb')
 
 function structurate(model = ensureArg('model'), connection = ensureArg('connection')) {
   return new Promise(async (resolve, reject) => {
     try {
       const dbList = await rethink.dbList().run(connection)
+
+      assert(model.databases, "Can't find 'databases'")
+      assert(Array.isArray(model.databases), "'databases' should be an array")
+
       for (const database of model.databases) {
+        assert(database.name, 'A database has no name')
         if (!~dbList.indexOf(database.name))
           await rethink.dbCreate(database.name).run(connection)
 
         const tbList = await rethink.db(database.name).tableList().run(connection)
+
+        assert(database.tables, "Can't find 'tables'")
+        assert(Array.isArray(database.tables), "'tables' should be an array")
+
         for (const table of database.tables) {
+          assert(table.name, 'A table has no name')
           if (!~tbList.indexOf(table.name))
             await rethink.db(database.name).tableCreate(table.name).run(connection)
 
           const idxList = await rethink.db(database.name).table(table.name).indexList().run(connection)
+          
+          assert(table.indexes, "Can't find 'tables'")
+          assert(Array.isArray(table.indexes), "'tables' should be an array")
+
           for (const index of table.indexes) {
             if (!~idxList.indexOf(index))
               await rethink.db(database.name).table(table.name).indexCreate(index).run(connection)
